dcfe54d98f27579c93a26aea96bc396b075f9dd3
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2017-06-23 17:15:15 -0400

    color scheme + hover effect

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,23 +2,26 @@ import React from 'react';
 
 const bannerStyles = {
   backgroundColor: 'rgb(7, 38, 29)',
-  color: 'white',
+  color: 'rgb(141, 241, 196)',
   height: '25vh',
   padding: '25px',
   paddingTop: '100px',
   fontSize: '8vmin',
+  boxSizing: 'border-box',
 };
+
 
 const getStyles = () => {
   return bannerStyles;
 };
-
 const Banner = () => {
   return (
     <div
       className="Banner"
       style={getStyles()}>
-      <div className="Banner-title">c l i x</div>
+
+      <div className="hvr-pulse">e l i x i r</div>
+
     </div>
   );
 };
